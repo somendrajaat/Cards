0a26b9a4b29dc3758546177f2e94b62b3bdebaf5
Somendra <somendrajaata@gmail.com>
2024-12-18 18:43:52 +0530

    done with final configuration

--- src/main/java/com/revisecard/Configuration/WebConfig.java
+++ src/main/java/com/revisecard/Configuration/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:63342")
+                .allowedOrigins("https://somendrajaat.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
